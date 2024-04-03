570d3c8ecd3ac4f4e5f6b8a6925cb3a3d0f28775
Ziad Elsabbagh <ziad.maged@hotmail.com>
2024-04-03 21:35:51 +0200

    Added the basic methods to the Graph Class

--- graph_db/src/main/java/com/server/graph_db/graphs/Graph.java
+++ graph_db/src/main/java/com/server/graph_db/graphs/Graph.java
@@ -1,17 +1,27 @@
 package com.server.graph_db.graphs;
+import com.server.graph_db.core.vertex.Edge;
+import com.server.graph_db.core.vertex.Vertex;
+import java.util.HashMap;
+import java.util.List;
 
 abstract public class Graph {
     private int nodes; // number of Shards or Servers
     private int vertices; // number of Vertices
     private int edges; // number of Edges
+    private HashMap<String, Vertex> vertexMap;
+    private HashMap<Vertex, List<Edge>> edgeMap;
 
     public Graph(){
         nodes = edges = vertices = 0;
+        vertexMap = new HashMap<>();
+        edgeMap = new HashMap<>();
     }
     public  Graph(int nodes, int vertices, int edges){
         this.nodes = nodes;
         this.vertices = vertices;
         this.edges = edges;
+        vertexMap = new HashMap<>(vertices);
+        edgeMap = new HashMap<>(edges);
     }
 
     public int getNodes(){
@@ -23,6 +33,26 @@ abstract public class Graph {
     public int getEdges(){
         return edges;
     }
+    public HashMap<String, Vertex> getVertexMap(){
+        return vertexMap;
+    }
+    public HashMap<Vertex, List<Edge>> getEdgeMap(){
+        return edgeMap;
+    }
+
+    public void addVertex(String id){
+        if(vertexMap.containsKey(id) || vertexMap.size() == vertices)
+            return;
+        vertexMap.put(id, new Vertex(id));
+    }
+
+    public void addVertex(String id, List<Edge> edges){
+        if(vertexMap.containsKey(id) || vertexMap.size() == vertices)
+            return;
+        Vertex vertex = new Vertex(id);
+        vertexMap.put(id, vertex);
+        edgeMap.put(vertex, edges);
+    }
 
     public void setNodes(int nodes){
         this.nodes = nodes;
@@ -32,6 +62,12 @@ abstract public class Graph {
     }
     public void setEdges(int edges){
         this.edges = edges;
+    }
+    public void setVertexMap(HashMap<String, Vertex> vertexMap){
+        this.vertexMap = vertexMap;
+    }
+    public void setEdgeMap(HashMap<Vertex, List<Edge>> edgeMap){
+        this.edgeMap = edgeMap;
     }
 
     abstract public boolean validate();
