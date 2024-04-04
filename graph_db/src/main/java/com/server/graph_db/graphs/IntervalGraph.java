925b6ec46d7ed2093457dd33797531c92af5f4ef
Ziad Elsabbagh <ziad.maged@hotmail.com>
2024-04-04 22:37:16 +0200

    Added the Interval object and implemented the validation method for the Interval Graph.

--- graph_db/src/main/java/com/server/graph_db/graphs/IntervalGraph.java
+++ graph_db/src/main/java/com/server/graph_db/graphs/IntervalGraph.java
@@ -1,8 +1,21 @@
 package com.server.graph_db.graphs;
 
+import com.server.graph_db.core.vertex.*;
+import com.server.graph_db.graphs.utilities.Interval;
+
 public class IntervalGraph extends Graph{
     @Override
     public boolean validate() {
-        return false;
+        for(Vertex v : this.getEdgeMap().keySet()){
+            if(!v.isPropertyExist("interval"))
+                return false;
+            Interval i = new Interval(v.getProperty("interval"));
+            for(Edge e : this.getEdgeMap().get(v)){
+                Interval j = new Interval(this.getVertexMap().get(e.getDestinationVertexId()).getProperty("interval"));
+                if(!i.overlaps(j))
+                    return false;
+            }
+        }
+        return true;
     }
 }
