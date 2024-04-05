867fa5751e8d15b971bc0e62df2219006dc44c3f
Ziad Elsabbagh <ziad.maged@hotmail.com>
2024-04-05 05:24:08 +0200

    Added the Validation method implementation for the Regular Graph.

--- graph_db/src/main/java/com/server/graph_db/graphs/RegularGraph.java
+++ graph_db/src/main/java/com/server/graph_db/graphs/RegularGraph.java
@@ -1,8 +1,31 @@
 package com.server.graph_db.graphs;
 
 public class RegularGraph extends Graph{
+
+    public RegularGraph(){
+        super();
+    }
+
+    public RegularGraph(int nodes, int vertices, int edges, GraphType type){
+        super(nodes, vertices, edges, type);
+    }
+
     @Override
     public boolean validate() {
-        return false;
+        if(this.getType() == GraphType.UNDIRECTED){
+            int degree = this.getDegreeOfVertex(this.getVertexMap().keySet().toArray()[0].toString());
+            for(String vertexID : this.getVertexMap().keySet()){
+                if(this.getDegreeOfVertex(vertexID) != degree)
+                    return false;
+            }
+        }else {
+            int inDegree = this.getInDegreeOfVertex(this.getVertexMap().keySet().toArray()[0].toString());
+            int outDegree = this.getOutDegreeOfVertex(this.getVertexMap().keySet().toArray()[0].toString());
+            for(String vertexID : this.getVertexMap().keySet()){
+                if(this.getInDegreeOfVertex(vertexID) != inDegree || this.getOutDegreeOfVertex(vertexID) != outDegree)
+                    return false;
+            }
+        }
+        return true;
     }
 }
