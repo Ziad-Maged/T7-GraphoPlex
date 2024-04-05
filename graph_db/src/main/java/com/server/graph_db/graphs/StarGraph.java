822af0b632755c419e542c8696f9bd95bdaa6f33
Ziad Elsabbagh <ziad.maged@hotmail.com>
2024-04-05 04:50:21 +0200

    Added the Validation method implementation for the Star Graph.

--- graph_db/src/main/java/com/server/graph_db/graphs/StarGraph.java
+++ graph_db/src/main/java/com/server/graph_db/graphs/StarGraph.java
@@ -1,8 +1,51 @@
 package com.server.graph_db.graphs;
+import com.server.graph_db.core.vertex.Vertex;
 
 public class StarGraph extends Graph{
+
+    public StarGraph(){
+        super();
+    }
+
+    public StarGraph(int nodes, int vertices, int edges, GraphType type){
+        super(nodes, vertices, edges, type);
+    }
+
     @Override
     public boolean validate() {
-        return false;
+        int numberOfVertices = this.getVertexMap().size();
+        Vertex center = null;
+        if(this.getType() == GraphType.UNDIRECTED){
+            for(String id : this.getVertexMap().keySet()){
+                if(this.getDegreeOfVertex(id) == numberOfVertices - 1){
+                    center = this.getVertexMap().get(id);
+                    break;
+                }
+            }
+            if(center == null)
+                return false;
+            for(String id : this.getVertexMap().keySet()){
+                if(!id.equals(center.getId()) && this.getDegreeOfVertex(id) != 1)
+                    return false;
+            }
+        }else {
+            for(String id : this.getVertexMap().keySet()){
+                int inDegree = this.getInDegreeOfVertex(id);
+                int outDegree = this.getOutDegreeOfVertex(id);
+                if(inDegree == numberOfVertices - 1 && outDegree == numberOfVertices - 1){
+                    center = this.getVertexMap().get(id);
+                    break;
+                }
+            }
+            if(center == null)
+                return false;
+            for(String id : this.getVertexMap().keySet()){
+                int inDegree = this.getInDegreeOfVertex(id);
+                int outDegree = this.getOutDegreeOfVertex(id);
+                if(!id.equals(center.getId()) && inDegree != 1 && outDegree != 1)
+                    return false;
+            }
+        }
+        return true;
     }
 }
