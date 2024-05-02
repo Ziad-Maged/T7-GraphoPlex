d1e4f3be9df87454acc4b6140cddc229adada73c
Ziad Elsabbagh <ziad.maged@hotmail.com>
2024-05-02 16:46:41 +0300

    Added match commands to the Graph API.

--- graph_db/src/main/java/com/server/graph_db/connection/GraphoPlexConnection.java
+++ graph_db/src/main/java/com/server/graph_db/connection/GraphoPlexConnection.java
@@ -181,6 +181,28 @@ public class GraphoPlexConnection {
         return this;
     }
 
+    public GraphoPlexConnection match(String parameters) throws IOException{
+        String jsonInputString = "{\"query\": \"MATCH " + parameters + "\"}";
+        try (OutputStream os = conn.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(conn.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim() + "\n");
+            }
+            System.out.println(response.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        conn.disconnect();
+        connect();
+        return this;
+    }
+
     public GraphoPlexConnection topologicalSort() throws IOException{
         String jsonInputString = "{\"query\": \"MATCH TOPOLOGICAL SORT" + "\"}";
         try (OutputStream os = conn.getOutputStream()) {
